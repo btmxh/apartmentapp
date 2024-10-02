29a4f8bc02eb17245162860b3bee7200b541f776
btmxh <ngoduyanh.chip@gmail.com>
2024-10-02 11:53:31 +0700

    Import instead of using fully qualified name

--- src/main/java/io/github/btmxh/apartmentapp/Announcement.java
+++ src/main/java/io/github/btmxh/apartmentapp/Announcement.java
@@ -1,9 +1,11 @@
 package io.github.btmxh.apartmentapp;
 
+import javafx.scene.control.Alert;
+
 public class Announcement {
 
     public static void show(String title, String header, String message) {
-        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(message);
